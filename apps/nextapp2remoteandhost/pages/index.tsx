544f0ecf404d5cff1afc97ca1cdfc9afd1c65dcc
Mike Pratt <mike@msjp.me>
2022-12-20 14:14:03 +0000

    Added shared ui package to next app 2, worked as expected with no modifications required.

--- apps/nextapp2remoteandhost/pages/index.tsx
+++ apps/nextapp2remoteandhost/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import Alert from '../components/Alert';
 import dynamic from 'next/dynamic';
+import { Button } from "ui";
 
 const ReviewsMicroFrontend = dynamic(() => import('Reviews/Home'), {
   ssr: false,
@@ -11,6 +12,7 @@ export default function Home() {
     <div>
       <p>Home page of next app 2</p>
       <Alert />
+      <Button>Yo yo yo UI Button in the house!</Button>
       <ReviewsMicroFrontend />
     </div>
   )
